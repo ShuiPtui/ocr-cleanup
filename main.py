babadb9c3854f63361797ff2ee58c0d69d90e30d
Trung <trung.bien@ontariotechu.net>
2023-12-06 21:55:03 -0500

    cleaned up code and did some postprocessing

--- main.py
+++ main.py
@@ -3,99 +3,13 @@ import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import nltk                            #will need to install
+from spellchecker import SpellChecker   #will need to install
+import string
+import re
 
-#Apparently pytesseract works best with PIL
-
-
-image1 = cv2.imread('black text.png')
-image2 = cv2.imread('stop-sign.jpg')
-imagePIL2 = Image.open('stop-sign.jpg')
-img_PIL2 = Image.fromarray(cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY))
-image3 = cv2.imread('store.jpeg')
-image4 = cv2.imread('drawing.png')
-image5a = cv2.imread('coffee store.jpg')
-image5b = cv2.imread('coffee store cropped.png') #If an image is cropped well, text detection of OCR works much better
-image5c = cv2.imread('coffeestorecropped.png') #has added metadata
-image6 = cv2.imread('drawing with noise.png')
-
-image1 = cv2.cvtColor(image1, cv2.COLOR_RGB2GRAY)
-image2 = cv2.cvtColor(image2, cv2.COLOR_RGB2GRAY)
-image3 = cv2.cvtColor(image3, cv2.COLOR_BGR2GRAY)
-image4 = cv2.cvtColor(image4, cv2.COLOR_BGR2GRAY)
-image5 = cv2.cvtColor(image5a, cv2.COLOR_BGR2GRAY)
-image6 = cv2.cvtColor(image6, cv2.COLOR_BGR2GRAY)
-
-#image processing before OCR to help with detecting text
-h, w = image5.shape[:2]
-image5 = cv2.resize(image5, (1000, int(h * (1000 / float(w)))))
-blurred_img1 = cv2.GaussianBlur(image5, (5,5), 0)
-edges_img1 = cv2.Canny(blurred_img1, 50, 150)
-ret, thresh1 = cv2.threshold(edges_img1, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-#pytesseract.image_to_boxes returns pretty useful information for replacing text 
-# <character> <left> <bottom> <right> <top> <confidence>
-
-
-boxes = pt.image_to_boxes(thresh1)
-# Parse bounding box coordinates
-bounding_boxes = [line.split()[1:5] for line in boxes.splitlines()]
-
-# Convert bounding box coordinates to integers
-bounding_boxes = [(int(x), int(y), int(w), int(h)) for (x, y, w, h) in bounding_boxes]
-
-# Calculate the crop region based on bounding boxes
-min_x = min(box[0] for box in bounding_boxes)
-min_y = min(box[1] for box in bounding_boxes)
-max_x = max(box[2] for box in bounding_boxes)
-max_y = max(box[3] for box in bounding_boxes)
-
-
-print('Min x: ' + str(min_x))
-print('Min y: ' + str(min_y))
-print('Max x: ' + str(max_x))
-print('Max y: ' + str(max_y))
-
-
-#pytesseract.image_to_data can provide more info
-data = pt.image_to_data(thresh1, output_type=pt.Output.DICT, config='--psm 11 --oem 3')
-num_boxes = len(data['level'])
-# for i in range(num_boxes):
-#     confidence = data['conf'][i]
-#     if confidence >= 30:
-#         print(confidence)
-#         (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
-#         crop = image5[y:y+h, x:x+w]
-#         pil_image = Image.fromarray(crop)
-
-#         blurred_img2 = cv2.GaussianBlur(np.array(pil_image), (5,5), 0)
-#         edges_img2 = cv2.Canny(blurred_img2, 50, 150)
-#         ret, thresh2 = cv2.threshold(edges_img2, 0, 255, cv2.THRESH_BINARY)
-
-
-
-#         print(pt.image_to_string(thresh2, lang='eng', config='--oem 3 --psm 8')) #Stop is read when I changed the config. I presume that the config is important
-#         plt.imshow(thresh2)
-#         plt.axis('off')  # Turn off axis labels
-#         plt.show()
-#         cv2.rectangle(image5a, (x, y), (x + w, y + h), (0, 255, 0), 2) #show boundary boxes
-
-
-# print('Data info: {}'.format(data))
-
-cv2.imshow('image', image5a) 
-
-
-  
-# waits for user to press any key 
-# (this is necessary to avoid Python kernel form crashing) 
-cv2.waitKey(0) 
-  
-
-# closing all open windows 
-cv2.destroyAllWindows() 
-
-print(pt.image_to_string(thresh1, lang='eng', config='--psm 11')) #Stop is read when I changed the config. I presume that the config is important
-
+nltk.download('brown') #downloads required set; slower than nltk.corpus.words but is able to deal with numbers and capitals
+#include 'pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>' if you don't have tesseract in your PATH
 
 # Page segmentation modes:
 #   0    Orientation and script detection (OSD) only.
@@ -114,5 +28,68 @@ print(pt.image_to_string(thresh1, lang='eng', config='--psm 11')) #Stop is read 
 #  13    Raw line. Treat the image as a single text line,
 #        bypassing hacks that are Tesseract-specific.
 
+def display_image(img):
+    cv2.imshow('Image', img)
+    cv2.waitKey()
 
+def image_preprocessing(img):
+    h, w = img.shape[:2]
+    img = cv2.resize(img, (1000, int(h * (1000 / float(w)))), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    blurred_img1 = cv2.GaussianBlur(img, (3,3), 0)
+    # edges_img1 = cv2.Canny(blurred_img1, 50, 150) #shows edges of the image
+    thresh = cv2.threshold(blurred_img1, 127, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C + cv2.THRESH_OTSU)[1] #threshold attributes may need to be changed
 
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+
+    #perform a morphological transformation
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+    dilation = cv2.dilate(thresh,kernel,iterations = 1)
+
+    #invert the image
+    invert = 255 - opening
+
+    return invert
+
+def image_ocr(img):
+    custom_config = r'--oem 3 --psm 6'
+    results = pt.image_to_string(img, lang='eng', config=custom_config)
+    print(results)
+    return results
+
+def check_results(text):
+    spell = SpellChecker()
+    corrected_line = ''
+    new_lines = []
+    split_pattern = re.compile(r'[' + re.escape(string.punctuation) + r'\s]+')
+
+    for line in text.splitlines():
+        for word in split_pattern.split(line):
+            
+            corrected = word_correction(spell, word)
+            print(word + ' ' + corrected)
+            if (valid_word(corrected)):
+                corrected_line += corrected.lower()
+                corrected_line += ' '
+        new_lines.append(corrected_line)
+        corrected_line = ''
+
+    return new_lines
+
+def valid_word(word):
+    return word.lower() in set(nltk.corpus.brown.words())
+
+def word_correction(spell, word):
+    if word.isspace() or word == '':
+        return word
+    else:
+        return spell.correction(word)
+    
+
+image = cv2.imread('coffee store.jpg')
+image = image_preprocessing(image)
+text = image_ocr(image)
+display_image(image)
+
+results = check_results(text) 
+print(results)
